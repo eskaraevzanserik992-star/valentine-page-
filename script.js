b759e86761ba36d0e9d3d826dd1255972987cf54
eskaraevzanserik992-star <eskaraevzanserik992@gmail.com>
2026-07-18 17:02:26 +0500

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const x = Math.random() * (window.innerWidth - noButton.offsetWidth);
         const y = Math.random() * (window.innerHeight - noButton.offsetHeight);
         
-        noButton.style.position = 'absolute';
+        noButton.style.position = 'absolute'; 
         noButton.style.left = `${x}px`;
         noButton.style.top = `${y}px`;
     }
